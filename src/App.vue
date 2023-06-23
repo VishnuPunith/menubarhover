954765af652acf54bdd637431e103ad23a428d3e
ramesh1919 <ramesh.avula@vistafoundation.co.in>
2023-06-23 15:50:04 +0530

    menubar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,147 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <body>
+    <div class="navigation">
+      <ul>
+        <li class="list">
+          <a href="#">
+            <span class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+</svg>
+            </span>
+            <span class="text">Home</span>
+          </a>
+         </li>
+         <li class="list">
+          <a href="#">
+            <span class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+</svg>
+            </span>
+            <span class="text">Profile</span>
+          </a>
+         </li>
+         <li class="list">
+          <a href="#">
+            <span class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+</svg>
+            </span>
+            <span class="text">Messages</span>
+          </a>
+         </li>
+         <li class="list">
+          <a href="#">
+            <span class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-images" viewBox="0 0 16 16">
+  <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+  <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
+</svg>
+            </span>
+            <span class="text">Photos</span>
+          </a>
+         </li>
+         <li class="list">
+          <a href="#">
+            <span class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+  <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+</svg>
+            </span>
+            <span class="text">Settings</span>
+          </a>
+         </li>
+      </ul>
+    </div>
+  </body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'typeface-poppins';
+export default{
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
 }
+
+  
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+<style scoped>
+body{
+  display:flex;
+  justify-content:center;
+  align-items: center;
+  font-family: 'Poppins', sans-serif;;
+  min-height: 100vh;
+  background: #31c499;
 }
+.navigation{
+  width: 400px;
+    height: 93px;
+    background: #9adb9f;
+    position: relative;
+    display: flex;
+    margin-top: -23pc;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    box-shadow: 0px 0px 5px 2px #000;
+    cursor: pointer;
+}
+
+.navigation ul{
+  display: flex;
+    width: 350px;
+    margin-top: 54px;
+    margin-left: -24px;
+}
+.navigation ul li{
+  position: relative;
+  list-style: none;
+  width:70px;
+  height:70px;
+  z-index:1;
+}
+.navigation ul li a {
+  position:relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction:coloumn;
+  width:100%;
+  text-align:center;
+  font-weight: 500;
+}
+.navigation ul li a .icon{
+  position:relative;
+  display: block;
+  line-height:75px;
+  font-size: 1.5rem;
+  text-align: center;
+ transition: 0.5s;
+ color:#222327;
+}
+.navigation ul li:hover a .icon{
+  transform: translateY(-35px);
+}
+.navigation ul li a .text{
+  position: absolute;
+  color:#222327;
+  font-weight: 400;
+  font-size: 0.75;
+  letter-spacing: 0.05rem;
+  transition:0.5s;
+  opacity: 0;
+  transform: translateY(20px);
+}
+.navigation ul li:hover a .text{
+  opacity: 1;
+  transform: translateY(10px);
+}
+
 </style>
